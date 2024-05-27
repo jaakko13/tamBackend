4be0b9f1018d530c63ff2eb3a8283d9970bacff3
jaakko <jaakko.j.virtanen@tuni.fi>
2024-05-27 12:44:20 +0300

    added content

--- tampBackend/api/models.py
+++ tampBackend/api/models.py
@@ -17,5 +17,8 @@ class Replies(models.Model):
     content = models.TextField()
     author = models.CharField(max_length=50)
     
+    def __str__(self):
+        return self.content
+    
     
 
