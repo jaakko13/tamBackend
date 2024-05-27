64d0059a920da73f66cb2e7b7c4f90a5b972bfe4
jaakko <jaakko.j.virtanen@tuni.fi>
2024-05-27 19:57:10 +0300

    B

--- tampBackend/settings.py
+++ tampBackend/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'tampbackend.urls'
+ROOT_URLCONF = 'tampBackend.urls'
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'tampbackend.wsgi.application'
+WSGI_APPLICATION = 'tampBackend.wsgi.application'
 
 
 # Database
